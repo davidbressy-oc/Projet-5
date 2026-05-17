1f4f331f9ba998f5ee7e14e456417a1663a7bda2
davidbressy-oc <david.bressy@gmail.com>
2026-05-17 13:41:49 +0200

    feat(carousel): complete step 4 implementation

--- assets/script.js
+++ assets/script.js
@@ -21,51 +21,37 @@ const slides = [
 /* Bullet points */
 let dotscontainer = document.querySelector(".dots");
 for (let i = 0; i < slides.length; i++) {
-	let dotspan = document.createElement("span");
-	dotspan.classList.add("dot");
-	if (i === 0) {
-		dotspan.classList.add("dot_selected");
-	}
-	dotscontainer.appendChild(dotspan);
+    let dotspan = document.createElement("span");
+    dotspan.classList.add("dot");
+    if (i === 0) {
+        dotspan.classList.add("dot_selected");
+    }
+    dotscontainer.appendChild(dotspan);
 }
 
 /* Flèches Slider */
 let arrow = document.querySelectorAll(".arrow");
+let dots = document.querySelectorAll(".dot");
+console.log(arrow)
 let currentSlide = 0;
 for (let i = 0; i < arrow.length; i++) {
-	arrow[i].addEventListener("click", () => {
-		if (i === 0) {
-			currentSlide--;
-		} else {
-			currentSlide++;
-		}
-		console.log(currentSlide);
-	});
+    arrow[i].addEventListener("click", () => {
+
+        if (i === 0) {
+            currentSlide--;
+
+
+        } else {
+            currentSlide++;
+        }
+
+        document.querySelector(".banner-img").src = `./assets/images/slideshow/${slides[currentSlide].image}`;
+        document.querySelector("#banner p").innerHTML = slides[currentSlide].tagLine;
+
+        for (let bullets = 0; bullets < dots.length; bullets++) {
+            dots[bullets].classList.remove('dot_selected')
+        }
+        dots[currentSlide].classList.add('dot_selected')
+        console.log(currentSlide);
+    });
 }
-
-
-
-
-/* Récupérer slides image et slides tagLine ?
-récupérer les bullet points ?
-
-dans le html
-banner img ===> slides image
-banner p ===> slides tagLine
-
-sortir dot selected de la boucle et le faire correspondre au currentSlide ?
-
-
-Au clic sur la flèche droite : 
-on change le bullet point actif au suivant ;
-on change l’image ;
-on change le texte correspondant à l’image.
-Au clic sur la flèche gauche, nous faisons la même chose mais pour les éléments précédents. 
-Recommandations : 
-
-Pour changer l’image, pensez à bien construire le chemin de la nouvelle image.
-Pour intégrer le texte, utilisez la propriété innerHTML et insérez la tagLine de la slide.
-Travaillez avec les index du tableau. 
-
-
-*/
